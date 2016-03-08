8d90a0dbc38a4cb6fe6be9f390d46a12be005dd3
Seto Elkahfi <setoelkahfi@gmail.com>
2016-03-08 17:57:13 +0700

    Object literals for string in Objective-C

--- Objective C/main.m
+++ Objective C/main.m
@@ -7,22 +7,18 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "Artist.h"
-#import "Album.h"
 
 int main(int argc, const char * argv[]) {
 
-    Artist *artist = [Artist artistWithName:@"The Beatles"];
-    artist.artistID = 1;
-    
-    NSLog(@"Artist %lu - %@", artist.artistID, artist.name);
-    
-    Album *rubberSoul = [[Album alloc] init];
-    rubberSoul.name = @"Rubber Soul";
-    Album *whiteAlbum = [[Album alloc] init];
-    whiteAlbum.name = @"White Album";
-    
-    [artist orderAlbum:rubberSoul quantity:1];
-    [artist orderAlbum:whiteAlbum quantity:2];
+    // Object literals
+    logh(@"Object Literals", ^{
+        
+        NSString *firstName = @"Paul";
+        NSString *lastName = @"McCartney";
+        
+        logs(firstName);
+        logs(lastName);
+        
+    });
     
 }
