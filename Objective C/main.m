071a86a0c28273c493db70289b9b34ba395b57f1
Seto Elkahfi <setoelkahfi@gmail.com>
2016-03-10 11:11:53 +0700

    Using NSNumbers in different ways

--- Objective C/main.m
+++ Objective C/main.m
@@ -7,22 +7,33 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "Artist.h"
-#import "Album.h"
 
 int main(int argc, const char * argv[]) {
 
-    Artist *artist = [Artist artistWithName:@"The Beatles"];
-    artist.artistID = 1;
+    BOOL validated = NO;
+    CGFloat width = 12.3;
+    NSInteger index = 2;
     
-    NSLog(@"Artist %lu - %@", artist.artistID, artist.name);
+    NSNumber *boxedBool = [NSNumber numberWithBool:validated];
+    NSNumber *boxedFloat = [NSNumber numberWithFloat:width];
+    NSNumber *boxedInteger = [NSNumber numberWithInteger:index];
     
-    Album *rubberSoul = [[Album alloc] init];
-    rubberSoul.name = @"Rubber Soul";
-    Album *whiteAlbum = [[Album alloc] init];
-    whiteAlbum.name = @"White Album";
+    NSLog(@"%@", boxedBool);
+    NSLog(@"%@", boxedFloat);
+    NSLog(@"%@", boxedInteger);
     
-    [artist orderAlbum:rubberSoul quantity:1];
-    [artist orderAlbum:whiteAlbum quantity:2];
+    BOOL unboxedBool = [boxedBool boolValue];
+    CGFloat unboxedFloat = [boxedFloat floatValue];
+    NSInteger unboxedInteger = [boxedInteger integerValue];
+    
+    NSLog(@"%@", unboxedBool ? @"YES" : @"NO");
+    NSLog(@"%f", unboxedFloat);
+    NSLog(@"%li", unboxedInteger);
+    
+    NSNumber *num = [NSNumber numberWithInteger:-12345];
+    NSLog(@"%@", num);
+    
+    NSUInteger unsignedInt = [num unsignedIntegerValue];
+    NSLog(@"%lu", unsignedInt);
     
 }
